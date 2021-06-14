283a384d7619a5a7546bdd992ed187eb65cbc1f8
mmccarthy64 <mmccarthy64@hotmail.com>
2021-06-14 16:35:02 -0500

    commit tree 1e1a2fc2559107680baf169a7c172eccdd575d2f

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className='navbar'>
+      < NavLink to='/'>Home</NavLink>
+      < NavLink to='/movies'>Movies</NavLink>
+      < NavLink to='/directors'>Directors</NavLink>
+      < NavLink to='/actors'>Actors</NavLink>
     </div>
   );
 };
